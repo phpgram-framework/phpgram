3fc19b21eb30dcf7208c356162efecb9351e3e06
Jörn Heinemann <j.heinemann1@web.de>
2019-09-08 00:13:25 +0200

    update tests

---
+++ test/Router/RouterTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Gram\Test\Router;
+use Gram\Route\Router;
+use PHPUnit\Framework\TestCase;
+
+class RouterTest extends TestCase
+{
+	public function testSimpleRoutes(){
+		$router= new Router(
+			
+		);
+	}
+}
