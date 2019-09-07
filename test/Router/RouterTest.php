a239432e7d0c9b02b86ea63687dc424e8b592602
Jörn Heinemann <j.heinemann1@web.de>
2019-09-08 00:34:19 +0200

    update tests

--- test/Router/RouterTest.php
+++ test/Router/RouterTest.php
@@ -1,13 +1,26 @@
 <?php
 namespace Gram\Test\Router;
 use Gram\Route\Router;
+use Nyholm\Psr7\Factory\Psr17Factory;
 use PHPUnit\Framework\TestCase;
 
 class RouterTest extends TestCase
 {
 	public function testSimpleRoutes(){
-		$router= new Router(
-			
-		);
+		$router= new Router();
+
+		$collector= $router->getCollector();
+
+		$factory= new Psr17Factory();
+
+		$uri = $factory->createUri('https://jo.com/test/vars/123/tester');
+
+		$collector->get('/test/vars/{var:n}/tester',"hallo");
+
+		$router->run($uri->getPath());
+
+		$handler = $router->getHandle();
+
+		self::assertEquals('hallo',$handler['callable']);
 	}
 }
