12fa56a7349f3885eadbc5f308bb99e97b3545d7
Jörn Heinemann <joernheinemann@gmx.de>
2020-02-19 00:08:24 +0100

    comments hinzugefügt

--- src/Route/Collector/RouteCollectorTrait.php
+++ src/Route/Collector/RouteCollectorTrait.php
@@ -24,50 +24,128 @@ use Gram\Route\RouteGroup;
  */
 trait RouteCollectorTrait
 {
+	/**
+	 * Füge eine Route mit beliebiger Method hinzu
+	 *
+	 * @param string $path
+	 * @param $handler
+	 * @param array $method
+	 * @return Route
+	 */
 	abstract function add(string $path,$handler,array $method):Route;
 
+	/**
+	 * Füge eine Gruppe hinzu
+	 *
+	 * @param $prefix
+	 * @param callable $groupcollector
+	 * @return RouteGroup
+	 */
 	abstract public function group($prefix,callable $groupcollector):RouteGroup;
 
+	/**
+	 * GET Route
+	 *
+	 * @param string $route
+	 * @param $handler
+	 * @return Route
+	 */
 	public function get(string $route,$handler):Route
 	{
 		return $this->add($route,$handler,['GET']);
 	}
 
+	/**
+	 * POST Route
+	 *
+	 * @param string $route
+	 * @param $handler
+	 * @return Route
+	 */
 	public function post(string $route,$handler):Route
 	{
 		return $this->add($route,$handler,['POST']);
 	}
 
+	/**
+	 * GET or POST Route
+	 *
+	 * @param string $route
+	 * @param $handler
+	 * @return Route
+	 */
 	public function getpost(string $route,$handler):Route
 	{
 		return $this->add($route,$handler,['GET','POST']);
 	}
 
+	/**
+	 * DELETE Route
+	 *
+	 * @param string $route
+	 * @param $handler
+	 * @return Route
+	 */
 	public function delete(string $route,$handler):Route
 	{
 		return $this->add($route,$handler,['DELETE']);
 	}
 
+	/**
+	 * PUT Route
+	 *
+	 * @param string $route
+	 * @param $handler
+	 * @return Route
+	 */
 	public function put(string $route,$handler):Route
 	{
 		return $this->add($route,$handler,['PUT']);
 	}
 
+	/**
+	 * PATCH Route
+	 *
+	 * @param string $route
+	 * @param $handler
+	 * @return Route
+	 */
 	public function patch(string $route,$handler):Route
 	{
 		return $this->add($route,$handler,['PATCH']);
 	}
 
+	/**
+	 * HEAD Route
+	 *
+	 * @param string $route
+	 * @param $handler
+	 * @return Route
+	 */
 	public function head(string $route,$handler):Route
 	{
 		return $this->add($route,$handler,['HEAD']);
 	}
 
+	/**
+	 * OPTIONS Route
+	 *
+	 * @param string $route
+	 * @param $handler
+	 * @return Route
+	 */
 	public function options(string $route,$handler):Route
 	{
 		return $this->add($route,$handler,['OPTIONS']);
 	}
 
+	/**
+	 * Route with every method
+	 *
+	 * @param string $route
+	 * @param $handler
+	 * @return Route
+	 */
 	public function any(string $route,$handler):Route
 	{
 		return $this->add($route,$handler,['GET','POST','DELETE','PUT','PATCH','HEAD','OPTIONS']);
