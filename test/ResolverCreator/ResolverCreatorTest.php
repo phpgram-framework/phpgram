d0be10ae6043b6c345953d0d83b734d7d3bbedb1
Jörn Heinemann <joernheinemann@gmx.de>
2020-08-29 12:21:48 +0200

    test resolver creator coverage erhöht

--- test/ResolverCreator/ResolverCreatorTest.php
+++ test/ResolverCreator/ResolverCreatorTest.php
@@ -4,9 +4,11 @@ namespace Gram\Test\ResolverCreator;
 use Gram\Resolver\CallableResolver;
 use Gram\Resolver\ClassResolver;
 use Gram\Resolver\ClosureResolver;
+use Gram\Resolver\HandlerResolver;
 use Gram\ResolverCreator\ResolverCreator;
 use Gram\Test\TestClasses\CallableClass;
 use Gram\Test\TestClasses\TestClass;
+use Gram\Test\TestClasses\TestHandlerClass;
 use PHPUnit\Framework\TestCase;
 
 class ResolverCreatorTest extends TestCase
@@ -61,5 +63,33 @@ class ResolverCreatorTest extends TestCase
 		}
 
 		self::assertInstanceOf(CallableResolver::class,$resolver);
+	}
+
+	public function testHandlerResolverCreation()
+	{
+		$toResolve = new TestHandlerClass();
+
+		try{
+			$resolver = $this->creator->createResolver($toResolve);
+		}catch (\Exception $e){
+			echo $e;
+			$resolver = null;
+		}
+
+		self::assertInstanceOf(HandlerResolver::class,$resolver);
+	}
+
+	public function testNotResolver()
+	{
+		$toResolve = new TestClass();
+
+		try{
+			$resolver = $this->creator->createResolver($toResolve);
+		}catch (\Exception $e){
+			//echo $e;
+			$resolver = null;
+		}
+
+		self::assertEquals(null,$resolver);
 	}
 }
